ecc529bf6962ef5ff5bf3d2ac6c88865181b0084
Cristian <Cristian@DESKTOP-04SDTSM>
2022-05-04 17:17:24 +0200

    versión 1.1

--- src/pradoejemplogit/PradoEjemploGit.java
+++ src/pradoejemplogit/PradoEjemploGit.java
@@ -7,7 +7,8 @@ public class PradoEjemploGit {
 
     public static void main(String[] args) {
         System.out.println("Hola, soy Cristian.");
-        System.out.println("Esta es la version 1.0");
+        System.out.println("Esta es la version 1.1");
+        System.out.println("Ya hice el primer commit");
     }
     
 }
