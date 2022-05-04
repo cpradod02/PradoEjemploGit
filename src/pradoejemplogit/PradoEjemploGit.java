c9984544378ecd642e3d5ba7729a1bfcd0e7ec68
Narza <Narza@DESKTOP-N39VQMD>
2022-05-04 18:31:00 +0200

    version final

--- src/pradoejemplogit/PradoEjemploGit.java
+++ src/pradoejemplogit/PradoEjemploGit.java
@@ -6,9 +6,10 @@ public class PradoEjemploGit {
 
 
     public static void main(String[] args) {
-        System.out.println("Hola, soy Cristian.");
+        System.out.println("Hola, soy Cristian -w-, Adrian es mejor que yo en todo.");
         System.out.println("Esta es la version 1.2");
         System.out.println("Esta es la version final.");
+        System.out.println("Troleado por Adrian");
         
     }
     
