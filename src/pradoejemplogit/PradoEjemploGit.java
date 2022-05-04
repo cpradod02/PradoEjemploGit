6e0bc63ef7b697017a188c26172e31deb157aea9
Cristian <Cristian@DESKTOP-04SDTSM>
2022-05-04 17:16:19 +0200

    versión 1.0

---
+++ src/pradoejemplogit/PradoEjemploGit.java
@@ -0,0 +1,13 @@
+
+package pradoejemplogit;
+
+
+public class PradoEjemploGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Cristian.");
+        System.out.println("Esta es la version 1.0");
+    }
+    
+}
